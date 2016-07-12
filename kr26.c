ca6b0cd5937d21d38e6d25e512f335bf3251dd57
jmxdbx <joelbny@gmail.com>
2016-07-12 14:54:49 -0700

    Update.

--- kr26.c
+++ kr26.c
@@ -1,3 +1,6 @@
+/* K&R C Exercise 2-6. Function setbits(x,p,n,y) returns x with the n bits that begin
+at position p set to the rightmost n bits of y. */
+
 #include <stdio.h>
 #include <string.h>
 
